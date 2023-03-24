8d8ac14d69f44e6ec825d67faf480539afb437e8
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-03-24 12:15:31 +0200

    [#04] Unity Animation Rigging - Aiming a weapon using Multi Aim Constraint

--- FinalProjectCity/Assets/Scripts/CharacterAiming.cs
+++ FinalProjectCity/Assets/Scripts/CharacterAiming.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
     public float turnSpeed = 15f;
+    public float aimDuration = 0.3f;
     private Camera mainCamera;
+    public Rig aimLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,17 @@ public class CharacterAiming : MonoBehaviour
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
