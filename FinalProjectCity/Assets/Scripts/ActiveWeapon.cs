58d63accf965c2cbd1e5b0b06cfbe2bf9c772730
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-03-29 11:41:56 +0200

    [#08] Unity Animation Rigging: Switching weapons using the Multi-Parent Constraint
    until 19:08

--- FinalProjectCity/Assets/Scripts/ActiveWeapon.cs
+++ FinalProjectCity/Assets/Scripts/ActiveWeapon.cs
@@ -16,7 +16,8 @@ public class ActiveWeapon : MonoBehaviour
     public Animator rigController;
     public Transform[] weaponSlots;
     
-    RaycastWeapon weapon;
+    RaycastWeapon[] equipped_weapons = new RaycastWeapon[2];
+    public int activeWeaponIndex;
     
 
     // Start is called before the first frame update
@@ -29,9 +30,19 @@ public class ActiveWeapon : MonoBehaviour
         }
     }
 
+    RaycastWeapon GetWeapon(int index)
+    {
+        if(index < 0 || index >= equipped_weapons.Length)
+        {
+            return null;
+        }
+        return equipped_weapons[index];
+    }
+
     // Update is called once per frame
     void Update()
     {
+        var weapon = GetWeapon(activeWeaponIndex);
         if(weapon)
         {
             weapon.UpdateWeapon(Time.deltaTime, GetIsHolstered());
@@ -46,20 +57,56 @@ public class ActiveWeapon : MonoBehaviour
     public void Equip(RaycastWeapon newWeapon)
     {
         int weaponSlotIndex = (int)newWeapon.weaponSlot;
+        var weapon = GetWeapon(weaponSlotIndex);
         if(weapon)
         {
             Destroy(weapon.gameObject);
         }
         weapon = newWeapon;
         weapon.raycastDestenation = crossHairTarget;
-        weapon.transform.parent = weaponSlots[weaponSlotIndex];
-        weapon.transform.localPosition = Vector3.zero;
-        weapon.transform.localRotation = Quaternion.identity;
-        rigController.Play("equip_" + weapon.weaponName);
+        weapon.transform.SetParent(weaponSlots[weaponSlotIndex], false);
+        equipped_weapons[weaponSlotIndex] = weapon;
+
+        SetActiveWeapon(weaponSlotIndex);
+    }
+
+    void SetActiveWeapon(int weaponSlotIndex)
+    {
+        int holsterIndex = activeWeaponIndex;
+        int activateIndex = weaponSlotIndex;
+        StartCoroutine(SwitchWeapon(holsterIndex, activateIndex));
     }
     
     private bool GetIsHolstered()
     {
         return rigController.GetBool("holster_weapon");
+    }
+
+    IEnumerator SwitchWeapon(int holsterIndex, int activateIndex)
+    {
+        yield return StartCoroutine(HolsterWeapon(holsterIndex));
+        yield return StartCoroutine(ActivateWeapon(activateIndex));
+        activeWeaponIndex = activateIndex;
+    }
+
+    IEnumerator HolsterWeapon(int index)
+    {
+        var weapon = GetWeapon(index);
+        if (weapon)
+        {
+            rigController.SetBool("holster_weapon", true);
+            yield return new WaitForSeconds(0.417f);
+        }
+    }
+
+    IEnumerator ActivateWeapon(int index)
+    {
+        var weapon = GetWeapon(index);
+        if (weapon)
+        {
+            rigController.SetBool("holster_weapon", false);
+            rigController.Play("equip_" + weapon.weaponName);
+            yield return new WaitForSeconds(0.417f);
+        }
     }
 }
