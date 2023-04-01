1a2b602cd2dabd34ba28b865e09c1bf7f444caa2
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-04-01 23:49:00 +0200

    fixed ui canvas for health bar

--- FinalProjectCity/Assets/Scripts/UiHealthBar.cs
+++ FinalProjectCity/Assets/Scripts/UiHealthBar.cs
@@ -5,23 +5,8 @@ using UnityEngine.UI;
 
 public class UiHealthBar : MonoBehaviour
 {
-
-    public Transform target;
     public Image foregroundImage;
-    public Image backgroundImage;
-    public Vector3 offset;
-
     
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 direction = (target.position - Camera.main.transform.position).normalized;
-        bool isBehind = Vector3.Dot(direction, Camera.main.transform.forward) <= 0.0f;
-        foregroundImage.enabled = !isBehind;
-        backgroundImage.enabled = !isBehind;
-        transform.position = Camera.main.WorldToScreenPoint(target.position + offset);
-    }
-
     public void SetHealthBarPercentage(float percentage)
     {
         float parentWidth = GetComponent<RectTransform>().rect.width;
