d2ef3291da9fbcda6eb2299935499890b2b89882
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-04-01 16:07:00 +0200

    Creating your first animated AI Character! [AI #01] #2 commit

--- FinalProjectCity/Assets/Scripts/AiLocomotion.cs
+++ FinalProjectCity/Assets/Scripts/AiLocomotion.cs
@@ -6,8 +6,12 @@ using UnityEngine.AI;
 public class AiLocomotion : MonoBehaviour
 {
     public Transform playerTransform;
+    public float maxTime = 1.0f;
+    public float maxDistance = 1.0f;
+
     private NavMeshAgent agent;
     private Animator animator;
+    private float timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +23,17 @@ public class AiLocomotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = playerTransform.position;
+        timer -= Time.deltaTime;
+        if(timer < 0.0f)
+        {
+            float sqDistance = (playerTransform.position - agent.destination).sqrMagnitude;
+            if(sqDistance > maxDistance * maxDistance)
+            {
+                agent.destination = playerTransform.position;
+            }
+            timer = maxTime;
+        }
+        
         animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 }
