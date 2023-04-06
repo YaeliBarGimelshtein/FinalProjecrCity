53f2e4096250d9bfcb5d7e1b17813a6c1de0d88f
Yaeli Bar Gimelshtein <yaeli47@gmail.com>
2023-04-06 20:25:37 +0200

    Unity AI shooting at the player [AI #06] #6 commit
    until 10:06

--- FinalProjectCity/Assets/Scripts/Health/PlayerHealth.cs
+++ FinalProjectCity/Assets/Scripts/Health/PlayerHealth.cs
@@ -1,29 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerHealth : Health
 {
     private Ragdoll ragdoll;
     private ActiveWeapon weapons;
     private CharacterAiming aiming;
+    private VolumeProfile postProcessing;
+    private CameraManager cameraManager;
 
     protected override void OnStart()
     {
         ragdoll = GetComponent<Ragdoll>();
         weapons = GetComponent<ActiveWeapon>();
         aiming = GetComponent<CharacterAiming>();
+        postProcessing = FindObjectOfType<Volume>().profile;
+        cameraManager = FindObjectOfType<CameraManager>();
     }
 
     protected override void OnDeath(Vector3 direction)
     {
         ragdoll.ActivateRagdoll();
+        direction.y = 1.0f;
+        ragdoll.ApplyForce(direction);
         weapons.DropWeapon();
         aiming.enabled = false;
+        cameraManager.EnableKillCam();
     }
 
     protected override void OnDamage(Vector3 direction)
     {
-
+        Vignette vignette;
+        if(postProcessing.TryGet(out vignette))
+        {
+            float percent = 1.0f - (currentHealth / maxHealth);
+            vignette.intensity.value = percent * 0.5f;
+        }
     }
 }
