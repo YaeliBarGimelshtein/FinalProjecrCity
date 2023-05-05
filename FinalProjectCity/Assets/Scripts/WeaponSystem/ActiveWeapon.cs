6db130f7f377207c8d00ddf24a36e6c03cc4cfd3
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-05-05 19:56:53 +0300

    Made some fixes to be abel to build the game

--- FinalProjectCity/Assets/Scripts/WeaponSystem/ActiveWeapon.cs
+++ FinalProjectCity/Assets/Scripts/WeaponSystem/ActiveWeapon.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
+#if UNITY_EDITOR
 using UnityEditor.Animations;
+#endif
+
+
 
 public class ActiveWeapon : MonoBehaviour
 {
